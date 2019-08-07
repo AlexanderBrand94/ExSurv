abb9700904659b1f9189f989c583c304508812fc
Alexander Brand <Alexander.Brand94@t-online.de>
2019-08-07 09:54:59 +0200

    Add files via upload

--- R/compare_fits_multiple.R
+++ R/compare_fits_multiple.R
@@ -11,16 +11,14 @@
 
 
 compare_fits_multiple <- function(formula_in, data = dat){
-  library(tidyverse)
-  library(survHE)
   model = fit.models(formula= formula_in, data = data,
                    distr=c("exponential","gompertz","weibull",
                            "weibullPH","loglogistic","lognormal"))
   list_model <- model$models %>% lapply(summary) %>% lapply(as.data.frame)
-  list_model <- bind_rows(list_model)
+  list_model <- dplyr::bind_rows(list_model)
   list_model$model <- rep(c("exponential","gompertz","weibull",
                           "weibullPH","loglogistic","lognormal"), each = nrow(list_model)/6)
-  ggplot(list_model, aes(time, est, color = as.factor(model))) +
+  ggplot2::ggplot(list_model, aes(time, est, color = as.factor(model))) +
     geom_ribbon(size = 1, aes(ymin = lcl, ymax = ucl,
                               color = NA, fill = as.factor(model)), alpha = 0.3)+ geom_line() +
     labs(color = "Model", x = "time", y = "survival", fill = "Model")+ theme_minimal() +
